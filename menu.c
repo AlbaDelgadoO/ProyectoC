cd830d3add950d95a450a32ba707eaca5aa55b8f
Oihane Camacho <oihane.camacho@opendeusto.es>
2024-04-06 13:42:44 +0200

    gestionPrestamosArreglado

--- menu.c
+++ menu.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "sqlite3.h"
-#include "database.h"
+#include "dataBase.h"
 #include "menu.h"
-#include "prestamos.h" 
 #include <string.h>
 
 void agregarNuevoLibro(sqlite3* db) {
@@ -191,6 +190,53 @@ void ejecutarMenuUsuarios(sqlite3* db) {
     } while (opcion != 6);
 }
 
+// GESTION DE PRESTAMOS
+
+void agregarNuevoPrestamo(sqlite3* db) {
+    // Código para agregar un nuevo préstamo
+}
+
+void buscarPrestamosPendientes(sqlite3* db) {
+    // Código para buscar préstamos pendientes
+}
+
+void mostrarMenuPrestamos() {
+    printf("=== Menu de Gestion de Prestamos ===\n");
+    printf("1. Agregar Nuevo Prestamo\n");
+    printf("2. Renovar Prestamo Existente\n");
+    printf("3. Registrar Devolucion de Libro\n");
+    printf("4. Gestionar Problemas de Prestamo\n");
+    printf("5. Volver al Menu Principal\n");
+    printf("Seleccione una opcion: ");
+}
+
+void ejecutarMenuPrestamos(sqlite3* db) {
+    int opcion;
+    do {
+        mostrarMenuPrestamos();
+        scanf("%d", &opcion);
+        switch (opcion) {
+            case 1:
+                agregarNuevoPrestamo(db);
+                break;
+            case 2:
+                //renovarPrestamo(db);
+                break;
+            case 3:
+                //registrarDevolucion(db);
+                break;
+            case 4:
+                //gestionarProblemasPrestamo(db);
+                break;
+            case 5:
+                printf("Volviendo al Menu Principal...\n");
+                break;
+            default:
+                printf("Opcion no valida. Por favor, seleccione una opcion valida.\n");
+                break;
+        }
+    } while (opcion != 5);
+}
 
 void mostrarMenuPrincipal() {
     printf("=== Menu Principal ===\n");
